3ccad3a344156bbe89c9664f0d2791eebae2f5ad
Enzostik <enzoalmenar13@gmail.com>
2023-12-02 14:17:31 -0300

    Nuevas páginas
    Buscador de servicios por tramo.
    Hacer reservas para tramos específicos

---
+++ ViajaPlus/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('search-service/', views.buscador_servicio, name='buscador_servicio'),
+    path('ver/<str:operation>/', views.buscar_info, name='buscar_servicio'),
+    path('ver/<str:operation>/<str:info>', views.buscar_info, name='buscar_servicio'),
+    path('reservation/<str:id>/<str:tramo>', views.reserva_pasajes, name='hacer_reserva'),
+    path('unit-status/<str:id>', views.mantenimiento_unidades, name='mantenimiento_unidades')
+]
